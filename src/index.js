035019648cdb06d8db892438939ec2beb3ae97a4
Steve Kinney <hello@stevekinney.net>
2019-11-15 10:38:32 -0800

    Live coding completed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
 import './styles.scss';
 
-const Application = () => {
+const inc = c => c + 1;
+const dec = c => c - 1;
+
+const Application = props => {
+  const [count, setCount] = useState(3);
+
+  const increment = () => setCount(inc);
+  const decrement = () => setCount(dec);
+
   return (
     <main className="Counter">
-      <p className="count">0</p>
+      <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Decrement</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
       </section>
     </main>
   );
 };
 
-render(<Application />, document.getElementById('root'));
+render(
+  <div>
+    <Application step={5} />
+    <Application step={15} />
+  </div>,
+  document.getElementById('root')
+);
